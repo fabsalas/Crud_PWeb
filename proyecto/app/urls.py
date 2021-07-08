1cc9a7b98a039d5bef210f9b8a2d5b45f4ec8cb7
Fabian <fab.salas@alumnos.duoc.cl>
2021-07-08 17:29:31 -0400

    Boton agregar funcionando

--- proyecto/app/urls.py
+++ proyecto/app/urls.py
@@ -1,6 +1,8 @@
-from app.views import inicio
+
+from app.views import inicio, agregar
 from django.urls import path
 
 urlpatterns = [
-    path('', inicio, name='indice'),
+    path('', inicio, name='inicio'),
+    path('agregar/', agregar, name='agregar'),
 ]
--- proyecto/app/views.py
+++ proyecto/app/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from app.models import Sistema, Smartphone
 # Create your views here.
 
 
 def inicio(request):
-    return render(request, 'inicio.html',{})
+    smartphones = Smartphone.objects.all()
+    sistemas = Sistema.objects.all()
+
+    contexto={'smartphones': smartphones, 'sistemas': sistemas} #con este diccionario se enviar las variables al html
+    return render(request, 'inicio.html', contexto)
+
+
+def agregar(request):
+    modelo =request.POST.get('modelo', '') #se obtiene el valor del input con name modelo.
+    id_sistema = request.POST.get('sistema', '') #se obtiene el valor de select con name sistema.
+
+    sistema = Sistema.objects.get(id=id_sistema) # obtenemos el objeto con ese id.
+
+    smartphone = Smartphone(modelo = modelo, sistema =sistema)
+    smartphone.save()
+
+    return redirect('inicio')
